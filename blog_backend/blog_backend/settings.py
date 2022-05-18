8606f846ee36656a145448f89768c77be7b9cbac
GianlucaSolinas <gianlucasolinas92@gmail.com>
2022-05-18 15:20:20 +0200

    add logging

--- blog_backend/blog_backend/settings.py
+++ blog_backend/blog_backend/settings.py
@@ -164,3 +164,21 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
