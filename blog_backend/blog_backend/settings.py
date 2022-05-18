dc0e74d7a34b1e3b6f6532d85a474e5e1661e634
GianlucaSolinas <gianlucasolinas92@gmail.com>
2022-05-18 14:11:55 +0200

    settings,py fix

--- blog_backend/blog_backend/settings.py
+++ blog_backend/blog_backend/settings.py
@@ -17,7 +17,6 @@ import django_heroku
 
 django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
