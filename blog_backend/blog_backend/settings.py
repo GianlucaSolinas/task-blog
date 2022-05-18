b12952b1e142e20ad578f5667b468a105e69629a
GianlucaSolinas <gianlucasolinas92@gmail.com>
2022-05-18 16:10:22 +0200

    CORS_ORIGIN_WHITELIST

--- blog_backend/blog_backend/settings.py
+++ blog_backend/blog_backend/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    'http://127.0.0.1:8000'
+    'http://127.0.0.1:8000',
+    'https://task-blog-greencast.web.app'
 ]
 
 MIDDLEWARE = [
