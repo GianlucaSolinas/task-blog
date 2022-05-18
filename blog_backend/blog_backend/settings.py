c9595baa0417a927470746823f2651adf1824eb2
GianlucaSolinas <gianlucasolinas92@gmail.com>
2022-05-18 14:13:59 +0200

    place heroku settings properly

--- blog_backend/blog_backend/settings.py
+++ blog_backend/blog_backend/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 import dj_database_url
 import django_heroku
 
-django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
@@ -170,3 +169,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
